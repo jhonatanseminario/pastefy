959c607930e94789e8ead41016007c36cd7f9d4e
Jhonatan Seminario <seminariodev@gmail.com>
2025-05-03 00:37:47 -0500

    feat(api): implement API functions for client creation and paste submission

---
+++ scripts/api.js
@@ -0,0 +1,43 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0/+esm";
+import { generateId } from "./utils.js";
+
+export function getClient() {
+    const supabaseUrl = "https://fbogwkdfwzdxdriwecbi.supabase.co";
+    const supabaseKey =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZib2d3a2Rmd3pkeGRyaXdlY2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0MjkzNTQsImV4cCI6MjA1MjAwNTM1NH0.XTfmP4M5eoVkWqhGgwlU7g_9kmlzj7WgrULLgkqkCEA";
+    const client = createClient(supabaseUrl, supabaseKey);
+
+    return client;
+}
+
+export async function sendPaste(pasteTitle, pasteText) {
+    if (!pasteText) {
+        return;
+    }
+
+    const pasteId = generateId();
+
+    try {
+        const { error } = await getClient()
+            .from("pastes")
+            .insert([{
+                content: pasteText,
+                title: pasteTitle,
+                slug: pasteId
+            }]);
+
+        if (error) {
+            console.error("Error al enviar el texto:", error);
+            return;
+        }
+
+        sessionStorage.setItem("copiedToClipboard", "true");
+        await navigator.clipboard.writeText(`${window.location.origin}/${pasteId}`);
+        window.location.href = `/${pasteId}`;
+
+    } catch (error) {
+        console.error("Error inesperado:", error);
+    }
+}
+
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,5 @@
-//!==========================================================================!//
-//!                             IMPORTAR MÓDULOS                             !//
-//!==========================================================================!//
-
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0/+esm";
-import { $, $$ } from './utils/dom.js';
+import { $, $$, isMobile } from './utils.js';
+import { getClient, sendPaste } from './api.js';
 
 
 //*==========================================================================*//
@@ -21,66 +17,7 @@ const pasteInput = $(".paste-input");
 const sendButton = $(".send-button");
 const notification = $(".notification");
 
-
-//*==========================================================================*//
-//*                CREAR Y DEVOLVER UNA INSTANCIA DE SUPABASE                *//
-//*==========================================================================*//
-
-function getClient() {
-    const supabaseUrl = "https://fbogwkdfwzdxdriwecbi.supabase.co";
-    const supabaseKey =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZib2d3a2Rmd3pkeGRyaXdlY2JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0MjkzNTQsImV4cCI6MjA1MjAwNTM1NH0.XTfmP4M5eoVkWqhGgwlU7g_9kmlzj7WgrULLgkqkCEA";
-    const client = createClient(supabaseUrl, supabaseKey);
-
-    return client;
-}
-
-
-//*==========================================================================*//
-//*                             CREAR NUEVO PASTE                            *//
-//*==========================================================================*//
-
-function generateId() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let id = '';
-    
-    for (let i = 0; i < 8; i++) {
-        const randomIndex = Math.floor(Math.random() * chars.length);
-        id += chars.charAt(randomIndex);
-    }
-    
-    return id;
-}
-
-async function sendPaste(pasteTitle, pasteText) {
-    if (!pasteText) {
-        return;
-    }
-
-    const pasteId = generateId();
-
-    try {
-        const { error } = await getClient()
-            .from("pastes")
-            .insert([{
-                content: pasteText,
-                title: pasteTitle,
-                slug: pasteId
-            }]);
-
-        if (error) {
-            console.error("Error al enviar el texto:", error);
-            return;
-        }
-
-        sessionStorage.setItem("copiedToClipboard", "true");
-        await navigator.clipboard.writeText(`${window.location.origin}/${pasteId}`);
-        window.location.href = `/${pasteId}`;
-
-    } catch (error) {
-        console.error("Error inesperado:", error);
-    }
-}
+sendPaste();
 
 
 //*==========================================================================*//
@@ -137,11 +74,6 @@ function renderPage(data) {
 }
 
 const slug = window.location.pathname.slice(1);
-
-function isMobile() {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent.toLowerCase());
-}
 
 async function fetchPaste(slug) {
     try {
