b5914f5e2ff6d22eb20ab5201b93b0092abbaa7c
Jhonatan Seminario <seminariodev@gmail.com>
2025-09-13 19:37:15 -0500

    refactor(dom): create DOM module for centralized element references

---
+++ src/js/dom.js
@@ -0,0 +1,15 @@
+import { $, $$ } from './utils.js';
+
+
+export const DOM = {
+    $main: $('main'),
+    $heroSection: $('#hero'),
+    $heroTitle: $('.title'),
+    $heroSubtitle: $('.subtitle'),
+    $pasteForm: $('.paste-form'),
+    $formLabels: $$('.label'),
+    $titleInput: $('.title-input'),
+    $pasteInput: $('.paste-input'),
+    $sendButton: $('.send-button'),
+    $notification: $('.notification'),
+}
